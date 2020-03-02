e8f1ca461ae38e9f892d611f9b130b9a2bcbd2e5
Ankizle <44819240+Ankizle@users.noreply.github.com>
2020-03-02 14:30:56 -0500

    added dataset generation

--- 2019-2020/index.cc
+++ 2019-2020/index.cc
@@ -4,10 +4,17 @@ using namespace std;
 
 int main() {
   
-  const int min_data = 70;
-  const int max_data = 10000;
+  const int min_data_size = 70
+    , max_data_size = 10000;
+  
+  const int min_int = 100
+    , max_int = 10000;
   
   vector<int> dataset;
+  
+  int data_size = rand() % (max_data_size - min_data_size + 1) + min_data_size;
+  
+  for (int i = 0; i < data_size; i++) dataset.push_back(rand() % (max_int - min_int + 1) + min_int);
   
   return 0;
 }
