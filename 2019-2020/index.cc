51b5443c24894b84813fe1665beb614b03f13ea6
Ankizle <44819240+Ankizle@users.noreply.github.com>
2020-03-02 14:23:27 -0500

    Update index.cc

--- 2019-2020/index.cc
+++ 2019-2020/index.cc
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
+  
+  const int min_data = 70;
+  const int max_data = 10000;
+  
+  vector<int> dataset;
+  
   return 0;
 }
